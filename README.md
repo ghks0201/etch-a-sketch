da87c1418a2e200e4a91717f9b7385bf1dffec28
ghks0201 <34975888+ghks0201@users.noreply.github.com>
2021-10-09 17:53:41 +0800

    Add color picker and update README

---
+++ README.md
@@ -0,0 +1,3 @@
+This is under the "Etch-A-Sketch" project from The Odin Project. Not a simple project, I have to spent 2 ~ 3 days to get all the things running. 
+There are quite a lot of repetitive code inside which I probably will revisit it some other time after I gain more knowlegde along the way.
+Nevertheless, this project is up and running and I am really proud of it.
--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const button = document.querySelector("button")
 const container = document.querySelector(".container")
+const colorPicker = document.querySelector("#color")
 
+
+// Generate new grid based on number of squares per side user entered
 function generateGrid() {
+
     let numberOfGrid = parseInt(prompt("How many squares per side do you want?"))
+
+    while (numberOfGrid > 100) {
+        numberOfGrid = parseInt(prompt("Number of squares per side must not exceed 100. Please reenter a number."))
+    }
+
+    let classDivs = document.querySelectorAll(".grid")
+
+    if (classDivs.length > 0) {
+        for (let i = 0; i < classDivs.length; i++) {
+            container.removeChild(classDivs[i])
+        }
+    }
+
     resetColor()
-    
+
     container.style.gridTemplateColumns = `repeat(${numberOfGrid}, 1fr)`
     container.style.gridTemplateRows = `repeat(${numberOfGrid}, 1fr)`
 
@@ -16,20 +33,32 @@ function generateGrid() {
             container.appendChild(div)
         }   
     }
+
     changeColor()
+    
 }
 
+// Change the color on hover
 function changeColor() {
     const grids = document.querySelectorAll(".grid")
+
+    // const rgbRed = Math.floor(Math.random() * 256)
+    // const rgbGreen = Math.floor(Math.random() * 256)
+    // const rgbBlue = Math.floor(Math.random() * 256)
+
+    // const rgbColor = `rgba(${rgbRed}, ${rgbGreen}, ${rgbBlue})`
+
 
     grids.forEach(function(grid) {
         grid.addEventListener("mouseover", () => {
             grid.classList.add("grid-on-hover")
+            
+            grid.style.backgroundColor = colorPicker.value
         })
     })
-    
 }
 
+// Reset color when new grid is created
 function resetColor() {
     const grids = document.querySelectorAll(".grid")
 
@@ -39,7 +68,7 @@ function resetColor() {
 }
 
 
+// Event listener for button to generate new grid
 button.addEventListener("click", () => {
     generateGrid()
 })
-
